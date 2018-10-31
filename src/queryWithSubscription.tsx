ce584c45a06726514eaa4a65965921ee75254d9d
z80 <muzo0@yandex.ru>
2018-10-31 23:16:15 +0300

    init

---
+++ src/queryWithSubscription.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Query, QueryProps, QueryResult, OperationVariables } from 'react-apollo';
+import { SubscribeToMoreOptions, DocumentNode } from 'apollo-boost';
+
+interface queryWithSubscriptionProps {
+  query: Pick<QueryProps, Exclude<keyof QueryProps, 'children'>> | string;
+  subscription: SubscribeToMoreOptions<OperationVariables, OperationVariables, OperationVariables> | string;
+  subscriptionDataToProps?: Function;
+  mutations?: { [key: string]: string };
+}
+
+class SubscriptionWrapper extends React.Component<{ subscribe: Function }> {
+  componentDidMount = () => this.props.subscribe();
+  render = () => this.props.children;
+}
+
+const defaultSubscriptionDataToProps = data => data;
+
+const defaultUpdateQuery = (subscriptionDataToProps: Function) => (prev, { subscriptionData }) => {
+  if (!subscriptionData.data) return prev;
+
+  return subscriptionDataToProps(subscriptionData.data);
+};
+
+export const queryWithSubscription = ({
+  query,
+  subscription,
+  mutations,
+  subscriptionDataToProps = defaultSubscriptionDataToProps,
+}: queryWithSubscriptionProps) => (Component: React.ComponentClass | (any) = Element) => {
+  const queryProps = typeof query === 'string' || query['kind'] ? { query: (query as any) as DocumentNode } : query;
+  const subscriptionOptions =
+    typeof subscription === 'string' || subscription['kind'] ? { document: subscription } : subscription;
+  const updateQuery = defaultUpdateQuery(subscriptionDataToProps);
+
+  return class extends React.Component {
+    static displayName = `ComponentWithSubscription(${'name' in Component ? Component.name : 'Anonymous'})`;
+
+    subscribe = ({ subscribeToMore }: QueryResult<OperationVariables>) => () =>
+      subscribeToMore({
+        updateQuery,
+        ...subscriptionOptions,
+      });
+
+    render = () => (
+      <Query {...queryProps}>
+        {result => (
+          <SubscriptionWrapper subscribe={this.subscribe(result)}>
+            <Component {...result} {...{ mutations }} />
+          </SubscriptionWrapper>
+        )}
+      </Query>
+    );
+  };
+};
